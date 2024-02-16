9ad8b32dcddcb13f44b308d7227c13268363ce9d
KillerDD13 <KillerDD013@gmail.com>
2024-02-15 18:51:06 -0600

    Set up the rest of the commands for the speed of the shooter

--- TheLaw2457_Crescendo/src/main/java/frc/robot/commands/Spit.java
+++ TheLaw2457_Crescendo/src/main/java/frc/robot/commands/Spit.java
@@ -6,24 +6,21 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.SpeedConstants;
-import frc.robot.subsystems.IntakeSubsystem;
-import frc.robot.subsystems.BeltDriveSubsystem;
-import frc.robot.subsystems.LiftSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class Spit extends Command {
 
-  private IntakeSubsystem INTAKE_SUBSYSTEM;
-  private double intakeSpeed;
+  private ShooterSubsystem SHOOTER_SUBSYSTEM;
+  private double shooterSpeed;
 
   /** Creates a new CubeSpit. */
-  public Spit(IntakeSubsystem intake, double speed) {
+  public Spit(ShooterSubsystem shooter, double speed) {
     // Use addRequirements() here to declare subsystem dependencies.
 
-    this.INTAKE_SUBSYSTEM = intake;
-    this.intakeSpeed = speed;
+    this.SHOOTER_SUBSYSTEM = shooter;
+    this.shooterSpeed = speed;
 
-    addRequirements(INTAKE_SUBSYSTEM);
+    addRequirements(SHOOTER_SUBSYSTEM);
   }
 
   // Called when the command is initially scheduled.
@@ -33,17 +30,13 @@ public class Spit extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (!INTAKE_SUBSYSTEM.distanceSensor.get()) {
-      INTAKE_SUBSYSTEM.set(intakeSpeed);
-    } else {
-      INTAKE_SUBSYSTEM.set(0);
-    }
+    
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    INTAKE_SUBSYSTEM.stop();
+    SHOOTER_SUBSYSTEM.stop();
   }
 
   // Returns true when the command should end.
