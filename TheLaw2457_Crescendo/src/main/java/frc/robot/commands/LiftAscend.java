2550b7837ae4a9f1394ce497b73b26939bf7683f
KillerDD13 <KillerDD013@gmail.com>
2024-02-15 18:51:41 -0600

    Set up commands for the Lift

---
+++ TheLaw2457_Crescendo/src/main/java/frc/robot/commands/LiftAscend.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.LiftSubsystem;
+import frc.robot.Constants.SpeedConstants;
+
+public class LiftAscend extends Command {
+
+  private LiftSubsystem LIFT_SUBSYSTEM;
+  private double liftSpeed;
+
+  /** Creates a new LiftAscend. */
+  public LiftAscend(LiftSubsystem lift, double speed) {
+    // Use addRequirements() here to declare subsystem dependencies.
+
+    this.LIFT_SUBSYSTEM = lift;
+    this.liftSpeed = speed;
+
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
